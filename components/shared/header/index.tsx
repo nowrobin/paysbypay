7024a5990fcf76adcfeb178ca8467264311bb49d
robin <nowrobin3@gmail.com>
2025-11-28 03:31:19 +0900

    refact: typo fix

--- components/shared/header/index.tsx
+++ components/shared/header/index.tsx
@@ -6,7 +6,7 @@ function Header() {
     <header className="fixed top-0 w-full h-12 px-14 z-20 bg-white py-2 flex flex-row justify-between items-center">
       <nav className="text-lg text-black flex flex-row  gap-3">
         <div className="text-xl font-bold">ALLPAYS</div>
-        <Link href="/dashboard">대시보드</Link>
+        <Link href="/">대시보드</Link>
         <Link href="/merchants">가맹점</Link>
         <Link href="/payments">개래 내역</Link>
       </nav>
